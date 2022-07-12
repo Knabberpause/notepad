78577508ca40a1e24d72f632be0ec2b86781e4b6
Seth Bailey <phskbailey@gmail.com>
2022-07-12 20:05:35 +0100

    V2,4 PRE-RELEASE

--- Notepad_2.4/notepad_2.4.py
+++ Notepad_2.4/notepad_2.4.py
@@ -1,14 +1,17 @@
 from os import fdopen as fd
 import os
-from tkinter import *
+import tkinter as tk
+
 from tkinter.ttk import *
 from tkinter import filedialog as fd
 from tkinter.filedialog import asksaveasfile
 from tkinter.filedialog import *
 from tkinter import messagebox
 from tkinter import ttk
+from tkinter import *
+from token import LEFTSHIFT
 
-root = Tk()
+root = tk.Tk()
 root.title("Notepad Basic")
 root.resizable(False, False)
 
@@ -23,23 +26,37 @@ def exitwosave():
 #config files located in appdata/config
 #config 1= saving
 #config2 - fonts
+#config3 - colours
 
-sfc_current_mes = StringVar()
-fontconfig = StringVar()
-global def_font
-def_font = ""
+sfc_current_mes = tk.StringVar()
+fontconfig = tk.StringVar()
 
 try:
     fontconfile = open("appdata/config/config2.txt", "rt")
+    colourconfile = open("appdata/config/config3.txt", 'rt')
     
     fontconfig = str(fontconfile.read())
     if fontconfig == "":
         fontconfile.write("Calibri")
+        
     else:
         pass
-    def_font=fontconfig
+
+    def_font = fontconfig
+    colourconfig = str(colourconfile.read())
+    if colourconfig == "":
+        colourconfile.write("")
+    else:
+        pass
+
 except:
     messagebox.showerror("Notepad Error Dialog", "Settings Preferences not found. Default settings applied.")
+    fontconfile = open("appdata/config/config2.txt", "wt")
+    colourconfile = open("appdata/config/config3.txt", 'x')
+    colourconfile.close()
+
+    fontconfile.write("Calibri")
+    fontconfile.close()
 
 
 def aboutapp():
@@ -54,42 +71,87 @@ def aboutapp():
     )
 
 
-def settingschangefont(scf_chosen):
-    fcsel = scf_chosen
-    print(fcsel)
+def sfc_courier_func():
     fontconfile = open("appdata/config/config2.txt", "w")
-    fontconfile.write(fcsel)
+    fontconfile.write("Courier")
     fontconfile.close()
+    messagebox.showinfo("Notepad Settings", "Please restart app for the changes to take effect")
 
-    def_font=fcsel
-    
+def sfc_bahnschrift_func():
+    fontconfile = open("appdata/config/config2.txt", "w")
+    fontconfile.write("Bahnschrift")
+    fontconfile.close()
+    messagebox.showinfo("Notepad Settings", "Please restart app for the changes to take effect")
+
+def sfc_calibri_func():
+    fontconfile = open("appdata/config/config2.txt", "w")
+    fontconfile.write("Calibri")
+    fontconfile.close()
+    messagebox.showinfo("Notepad Settings", "Please restart app for the changes to take effect")
+
+def scc_red_func():
+    colourconfile = open("appdata/config/config3.txt", "w")
+    colourconfile.write("Red")
+    colourconfile.close()
+    messagebox.showinfo("Notepad Settings", "Please restart app for the changes to take effect")
+
+def scc_def_func():
+    colourconfile = open("appdata/config/config3.txt", "w")
+    colourconfile.write("")
+    colourconfile.close()
+    messagebox.showinfo("Notepad Settings", "Please restart app for the changes to take effect")
 
 def settings():
-    settingswin = Tk()
+    settingswin = tk.Tk()
     settingswin.title("Settings")
     settingswin.iconbitmap("appdata/appicon.ico")
+
+
     setnotebook = ttk.Notebook(settingswin)
     setnotebook.pack(pady=10, expand=True)
-    
+        
     set_fonts = ttk.Frame(setnotebook, width=400, height=300)
     setnotebook.add(set_fonts, text="Fonts")
 
-    #FONTS PAGE
-    sfc_courier = Button(set_fonts, text="Courier", font="Courier 15",command=settingschangefont('Courier'))
-    sfc_courier.pack()
+    set_colours = ttk.Frame(setnotebook, width=400, height=300)
+    setnotebook.add(set_colours, text="Colours")
 
-    sfc_bahnschrift = Button(set_fonts, text="Bahnschrift", font="Bahnschrift Light 15", command=settingschangefont('Bahnschrift'))
+    #FONTS PAGE
+    sfc_courier = tk.Button(set_fonts, text='Courier', command=sfc_courier_func)
+    sfc_bahnschrift = tk.Button(set_fonts, text='Bahnschrift', command=sfc_bahnschrift_func)
+    sfc_calibri = tk.Button(set_fonts, text='Calibri', command=sfc_calibri_func)
+    sfc_calibri.pack()
+    sfc_courier.pack()
     sfc_bahnschrift.pack()
 
+    #colour scheme page
+    #button colour section
+    scc_label = tk.Label(set_colours, text="BUTTON COLOURS")
+    scc_label.pack()
+
+    scc_red =  tk.Button(set_colours, text="Red", fg="Red", command=scc_red_func)
+    scc_red.pack()
+
+    scc_grey =  tk.Button(set_colours, text="Grey (Default)", fg="Dark Grey", command=scc_def_func)
+    scc_grey.pack()
     settingswin.mainloop()
+
 
 #PREREQUISITE VARIABLES
 
 saved = "no"
 
 try:
-    sbphoto = PhotoImage(file = r'appdata/saveb.png')
-    obphoto = PhotoImage(file = r"appdata/openb.png")
+    config3file = open('appdata/config/config3.txt', 'r')
+    config3 = config3file.read()
+    if config3 == "Red":
+        sbphoto = tk.PhotoImage(file = r'appdata/alt_buttons/saveb_red.png')
+        obphoto = tk.PhotoImage(file = r"appdata/alt_buttons/openb_red.png")
+    else:
+        sbphoto = tk.PhotoImage(file = r'appdata/saveb.png')
+        obphoto = tk.PhotoImage(file = r"appdata/openb.png")
+
+    
     root.iconbitmap("appdata/appicon.ico")
 except:
     messagebox.showerror("NOTEPAD ERROR DIALOG", """
@@ -102,19 +164,42 @@ except:
 
 #PAGE PREREQ DEFINES
 page1 = ttk.Frame(notebook, width=650, height=400)
-pad1 = Text(page1, bg='#aca8b7', font=def_font)
+pad1 = tk.Text(page1, bg='#aca8b7', font=def_font)
 
 page2 = ttk.Frame(notebook, width=650, height=400)
-pad2 = Text(page2, bg='#aca8b7', font=def_font)
+pad2 = tk.Text(page2, bg='#aca8b7', font=def_font)
 
 page3 = ttk.Frame(notebook, width=650, height=400)
-pad3 = Text(page3, bg='#aca8b7', font=def_font)
+pad3 = tk.Text(page3, bg='#aca8b7', font=def_font)
 
 global pad4
 page4 = ttk.Frame(notebook, width=650, height=400)
-pad4 = Text(page4, bg='#aca8b7', font=def_font)
-sf4 = StringVar()
-sf4data =StringVar()
+pad4 = tk.Text(page4, bg='#aca8b7', font=def_font)
+sf4 = tk.StringVar()
+sf4data = tk.StringVar()
+
+def OpenFile():
+    try:
+        fdopen = fd.askopenfilename(initialdir='/', title="select file",
+                                      filetypes=(("Text Files",".txt"), ("All Files","*.*")))
+        openfile = open(fdopen)
+        opendata = openfile.read()
+        global sf4
+        sf4 = fdopen
+        pad4.insert(END, opendata)
+
+        page4.pack(fill='both', expand=True)
+        
+        pad4.pack()
+        notebook.add(page4, text=fdopen)
+
+        savebutton4 = tk.Button(page4, image = sbphoto, command = SaveFile4)
+        savebutton4.pack(side=LEFT)
+        openbutton4 = tk.Button(page4, image=obphoto, command = OpenFile)
+        openbutton4.pack(side=LEFT)
+    except:
+        if messagebox.showerror("Notepad Error Dialog", "There has been an error in the application"):
+            pass
 
 def SaveFile1():
     f = asksaveasfile(mode='w', defaultextension=".txt")
@@ -124,8 +209,9 @@ def SaveFile1():
     f.write(text)
     f.close() # `()` was missing.
 
-    configfile = open('config1.txt', 'w')
+    configfile = open('appdata/config1.txt', 'w')
     configfile.write('y')
+    configfile.close()
 
 def SaveFile2():
     f = asksaveasfile(mode='w', defaultextension=".txt")
@@ -135,7 +221,7 @@ def SaveFile2():
     f.write(text)
     f.close() # `()` was missing.
 
-    configfile = open('config1.txt', 'w')
+    configfile = open('appdata/config1.txt', 'w')
     configfile.write('y')
 
 def SaveFile3():
@@ -146,7 +232,7 @@ def SaveFile3():
     f.write(text)
     f.close() # `()` was missing.
 
-    configfile = open('config1.txt', 'w')
+    configfile = open('appdata/config1.txt', 'w')
     configfile.write('y')
 
 def SaveFile4():
@@ -164,34 +250,11 @@ def SaveFile4():
 
 root.protocol("WM_DELETE_WINDOW", exitwosave)
 
-def OpenFile():
-    try:
-        fdopen = fd.askopenfilename(initialdir='/', title="select file",
-                                      filetypes=(("Text Files",".txt"), ("All Files","*.*")))
-        openfile = open(fdopen)
-        opendata = openfile.read()
-        global sf4
-        sf4 = fdopen
-        pad4.insert(END, opendata)
 
-        page4.pack(fill='both', expand=True)
-        
-        pad4.pack()
-        notebook.add(page4, text=fdopen)
-
-        savebutton4 = Button(page4, image = sbphoto, command = SaveFile4)
-        savebutton4.pack(side=LEFT)
-        openbutton4 = Button(page4, image=obphoto, command = OpenFile)
-        openbutton4.pack(side=LEFT)
-    except:
-        if messagebox.showerror("Notepad Error Dialog", "There has been an error in the application"):
-            pass
-
-
-menubar = Menu(root, activebackground="white")
+menubar = tk.Menu(root, activebackground="white")
 root.config(menu=menubar)
 
-filemenu = Menu(menubar, tearoff=0)
+filemenu = tk.Menu(menubar, tearoff=0)
 filemenu.add_command(label='Open File', command=OpenFile)
 filemenu.add_command(label='About', command=aboutapp)
 
@@ -200,7 +263,7 @@ menubar.add_cascade(
     menu=filemenu
 )
 
-padmenu = Menu(menubar, tearoff=0)
+padmenu = tk.Menu(menubar, tearoff=0)
 padmenu.add_command(label='Save Notebook 1', command=SaveFile1)
 padmenu.add_command(label='Save Notebook 2', command=SaveFile2)
 padmenu.add_command(label='Save Notebook 3', command=SaveFile3)
@@ -220,10 +283,10 @@ menubar.add_command(label="Settings", command=settings)
 page1.pack(fill='both', expand=True)
 pad1.pack()
 notebook.add(page1, text='Notebook 1')
-savebutton1 = Button(page1, image = sbphoto, command = SaveFile1)
+savebutton1 = tk.Button(page1, image = sbphoto, command = SaveFile1)
 savebutton1.pack(side=LEFT)
 
-openbutton1 = Button(page1, image=obphoto, command = OpenFile)
+openbutton1 = tk.Button(page1, image=obphoto, command = OpenFile)
 openbutton1.pack(side=LEFT)
 
 
@@ -231,10 +294,10 @@ openbutton1.pack(side=LEFT)
 page2.pack(fill='both', expand=True)
 pad2.pack()
 notebook.add(page2, text='Notebook 2')
-savebutton2 = Button(page2, image = sbphoto, command = SaveFile2)
+savebutton2 = tk.Button(page2, image = sbphoto, command = SaveFile2)
 savebutton2.pack(side=LEFT)
 
-openbutton2 = Button(page2, image=obphoto, command = OpenFile)
+openbutton2 = tk.Button(page2, image=obphoto, command = OpenFile)
 openbutton2.pack(side=LEFT)
 
 
@@ -242,10 +305,10 @@ openbutton2.pack(side=LEFT)
 page3.pack(fill='both', expand=True)
 pad3.pack()
 notebook.add(page3, text='Notebook 3')
-savebutton3 = Button(page3, image = sbphoto, command = SaveFile3)
+savebutton3 = tk.Button(page3, image = sbphoto, command = SaveFile3)
 savebutton3.pack(side=LEFT)
 
-openbutton3 = Button(page3, image=obphoto, command = OpenFile)
+openbutton3 = tk.Button(page3, image=obphoto, command = OpenFile)
 openbutton3.pack(side=LEFT)
 
 
